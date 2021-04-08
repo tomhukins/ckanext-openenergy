8684820e46b5852a2385541f5c7d7048b315776e
Tom Hukins <tom.hukins@icebreakerone.org>
2021-04-08 11:42:07 +0100

    Disable non-working code

--- ckanext/openenergy/plugin.py
+++ ckanext/openenergy/plugin.py
@@ -9,8 +9,8 @@ from ckan.plugins import implements, SingletonPlugin
 from ckan.plugins import IConfigurer
 from ckan.plugins import IRoutes
 
-from ckan.lib.base import render
-import ckan.plugins as plugins
+#from ckan.lib.base import render
+#import ckan.plugins as plugins
 
 import routes.mapper
 
@@ -91,9 +91,9 @@ class OpenenergyPlugin(SingletonPlugin):
         return route_map
 
 
-class ExtraPagesController(plugins.toolkit.BaseController):
-    def changelog(self):
-        return render('changelog.html')
-    
-    def request_data(self):
-        return render('request_data.html')
+#class ExtraPagesController(plugins.toolkit.BaseController):
+#    def changelog(self):
+#        return render('changelog.html')
+#
+#    def request_data(self):
+#        return render('request_data.html')
